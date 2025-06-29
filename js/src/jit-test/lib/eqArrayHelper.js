5f7ef5346734b0a055caed7b95fa36a9a6e3dedd
Rong "Mantle" Bao <webmaster@csmantle.top>
2025-06-29 17:14:06 +0800

    LOCAL - jit-test/lib - Add replacer for unserializable BigInt
    The implementation is attributed to
    <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt#use_within_json>.

--- js/src/jit-test/lib/eqArrayHelper.js
+++ js/src/jit-test/lib/eqArrayHelper.js
@@ -3,17 +3,22 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 function assertEqArray(actual, expected) {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt#use_within_json
+    const bigIntReplacer = (key, value) =>
+        typeof value === "bigint" ? { $bigint: value.toString() } : value;
+
+
     if (actual.length != expected.length) {
         throw new Error(
             "array lengths not equal: got " +
-            JSON.stringify(actual) + ", expected " + JSON.stringify(expected));
+            JSON.stringify(actual, bigIntReplacer) + ", expected " + JSON.stringify(expected, bigIntReplacer));
     }
 
     for (var i = 0; i < actual.length; ++i) {
         if (actual[i] != expected[i]) {
         throw new Error(
             "arrays not equal at element " + i + ": got " +
-            JSON.stringify(actual) + ", expected " + JSON.stringify(expected));
+            JSON.stringify(actual, bigIntReplacer) + ", expected " + JSON.stringify(expected, bigIntReplacer));
         }
     }
 }
