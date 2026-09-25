b52ae7ca257e578d1e0215879250497ad555a4ad
Valentin Pollet <vpollet@mozilla.com>
2026-09-25 15:22:44 +0000

    Bug 2046983 - Part 4: Add host process to ML telemetry r=gregtatum,ai-platform-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D324722

--- toolkit/components/ml/textgeneration/TextGenerationEngine.sys.mjs
+++ toolkit/components/ml/textgeneration/TextGenerationEngine.sys.mjs
@@ -307,6 +307,7 @@ export class TextGenerationEngine {
     this.telemetry = new lazy.MLTelemetry({
       featureId: pipelineOptions.featureId,
       flowId: pipelineOptions.flowId,
+      hostProcess: "hwinference",
     });
   }
 
